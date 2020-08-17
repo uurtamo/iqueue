44d0ff1af3291282296f963ff4083400ad198f7d
Steve Uurtamo <uurtamo@gmail.com>
2020-08-17 10:01:34 -0700

    fixed package

--- iqueue.go
+++ iqueue.go
@@ -2,60 +2,54 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package "iqueue"
+package iqueue
 
 import (
-
-	//	"fmt"
 	"sync"
 )
 
 type Interval struct {
-
 	Lower uint32
 	Upper uint32
 }
 
 type node struct {
-
 	value *Interval
-	next *node
+	next  *node
 }
 
-
 type Queue struct {
-
 	hmux sync.Mutex
 	tmux sync.Mutex
 	head *node
 	tail *node
 }
 
-func (Q *Queue) Init () *Queue {
+func (Q *Queue) Init() *Queue {
 
 	i_node := new(node)
-	i_node.next=nil
-	Q.head=i_node
-	Q.tail=i_node
+	i_node.next = nil
+	Q.head = i_node
+	Q.tail = i_node
 	return Q
 }
 
-func (Q *Queue) Enqueue (bounds *Interval) {
+func (Q *Queue) Enqueue(bounds *Interval) {
 
 	t_node := new(node)
-	t_node.value=bounds
-	t_node.next=nil
+	t_node.value = bounds
+	t_node.next = nil
 
 	Q.tmux.Lock()
 
-	Q.tail.next=t_node
-	Q.tail=t_node
+	Q.tail.next = t_node
+	Q.tail = t_node
 
 	Q.tmux.Unlock()
 	return
 }
 
-func (Q *Queue) Dequeue () *Interval {
+func (Q *Queue) Dequeue() *Interval {
 
 	Q.hmux.Lock()
 	defer Q.hmux.Unlock()
