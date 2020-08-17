72430b862dc8f46b82f8a0d2ef7a6d8531a7a6ad
Steve Uurtamo <uurtamo@gmail.com>
2020-08-17 09:43:43 -0700

    update

--- iqueue.go
+++ iqueue.go
@@ -1,3 +1,7 @@
+// Copyright 2020 Steve Uurtamo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package "github.com/uurtamo/iqueue"
 
 import (
